3914ed05ee8e3ca09a0f541f5297fe0ca85a4eaf
yodacode <benjamin.devaublanc@gmail.com>
2014-03-10 23:15:24 +0100

    add routes create multiple views

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
-var pokedexApp = angular.module('pokedexApp', [  
-  'pokemonControllers'
+var pokedexApp = angular.module('pokedexApp', [
+	'ngRoute',
+	'pokemonControllers'
 ]);
+
+pokedexApp.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider
+			.when('/', {templateUrl: 'partials/list.html'})
+			.when('/detail/:name', {templateUrl: 'partials/detail.html'})
+			.otherwise({redirectTo: '/'})
+
+}]);
+
--- js/controllers.js
+++ js/controllers.js
@@ -1,12 +1,18 @@
 var pokemonControllers = angular.module('pokemonControllers', []);
 
-
 pokemonControllers.controller('pokemonList', ['$scope', '$http',
   function ($scope, $http) {
   	$scope.loading = true;
     $http.get('api/pokedex.php').success(function(data) {
+    	console.log(data.pokemon);
     	$scope.pokemons = data.pokemon;
+
     	$scope.loading = false;
-    }); 
-    
+    });
+
+}]);
+
+pokemonControllers.controller('pokemonDetail', ['$scope', '$http',
+	function ($scope, $http) {
+  	
 }]);
