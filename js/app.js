3059d91de3d5ab040e26eedff67ba6a597441488
yodacode <benjamin.devaublanc@gmail.com>
2014-03-12 09:30:38 +0100

    listing and detail done

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ pokedexApp.config(['$routeProvider',
 	function($routeProvider) {
 		$routeProvider
 			.when('/', {templateUrl: 'partials/list.html'})
-			.when('/detail/:name', {templateUrl: 'partials/detail.html'})
+			.when('/detail/:id', {templateUrl: 'partials/detail.html'})
 			.otherwise({redirectTo: '/'})
 
 }]);
--- js/controllers.js
+++ js/controllers.js
@@ -1,18 +1,32 @@
 var pokemonControllers = angular.module('pokemonControllers', []);
 
 pokemonControllers.controller('pokemonList', ['$scope', '$http',
-  function ($scope, $http) {
+  	function ($scope, $http) {
+  	var id;
+
   	$scope.loading = true;
     $http.get('api/pokedex.php').success(function(data) {
-    	console.log(data.pokemon);
+    	for (var i=0; i < data.pokemon.length; i++) {
+    		id = data.pokemon[i]['resource_uri'].split('/')[3];
+    		data.pokemon[i].id = id;
+    	}
     	$scope.pokemons = data.pokemon;
-
     	$scope.loading = false;
     });
 
 }]);
 
-pokemonControllers.controller('pokemonDetail', ['$scope', '$http',
-	function ($scope, $http) {
-  	
+pokemonControllers.controller('pokemonDetail', ['$scope', '$http', '$routeParams',
+	function ($scope, $http, $routeParams) {
+		$scope.loading = true;
+
+		$http.get('api/pokemon.php', {
+		    params: { id: $routeParams.id }
+		}).success(function (data) {
+
+  			$scope.pokemon = data;
+  			$scope.moves = data.moves;
+  			$scope.loading = false;
+
+		});
 }]);
