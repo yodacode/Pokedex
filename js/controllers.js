039a2416e66fd0c7b6a2c1f4ef29b4a9d9fddc82
yodacode <benjamin.devaublanc@gmail.com>
2014-03-14 14:37:18 +0100

    add keyboard controls to navigate in the list

--- js/controllers.js
+++ js/controllers.js
@@ -1,39 +1,99 @@
 var pokemonControllers = angular.module('pokemonControllers', []);
 
-//shared datas between controller
-
 pokemonControllers.controller('pokemonHome', ['$scope', '$http',
-	function ($scope, $http) { 
-
-    //init template
-        
+	function ($scope, $http) {
 
     $scope.getId = function (uri) {
       id = uri.split('/')[3];
       return id;
     }
-    
-    $scope.select= function (i) {
-      $scope.selectedIndex = i;
+
+    $scope.submit = function () {
+      var focusedId;
+      if ($scope.pokemons[$scope.selectedIndex]) {
+        focusedId = $scope.pokemons[$scope.selectedIndex].resource_uri.split('/')[3];
+        $scope.getPokemon(focusedId);
+      } else {
+        return false;
+      }
+    }
+
+    $scope.select= function (index) {
+      $scope.selectedIndex = index;
     };
 
+    $scope.selectUp = function (index) {
+      if (index === undefined || index < 0) {
+        $scope.selectedIndex = 0;
+      } else {
+        $scope.selectedIndex -= 1;
+      }
+    }
+
+    $scope.selectDown = function (index) {
+      if (index === undefined) {
+        $scope.selectedIndex = 0;
+      } else {
+        $scope.selectedIndex += 1;
+      }
+    }
+
     $scope.getPokemon = function (id) {
-      
+
       $scope.loader = true;
       $scope.displayDetail = false;
 
       $http.get('api/pokemon.php', {
-          params: { id: id }
-      }).success(function (data) {        
-          $scope.pokemon = data; 
-          $scope.loader = false;
-          $scope.abilities = data.abilities;
-          $scope.displayDetail = true;
+        params: { id: id }
+      }).success(function (data) {
+        $scope.pokemon = data;
+        $scope.loader = false;
+        $scope.abilities = data.abilities;
+        $scope.displayDetail = true;
       });
     }
 
-		$http.get('api/pokedex.php').success(function(data) {      
+		$http.get('api/pokedex.php').success(function(data) {
       $scope.pokemons = data.pokemon;
     });
 	}
 ]);
+
+pokemonControllers.directive('ngArrowup', function () {
+    return function (scope, element, attrs) {
+      element.bind("keydown keypress", function (event) {
+        if(event.which === 38) {
+          scope.$apply(function (){
+            scope.$eval(attrs.ngArrowup);
+          });
+          event.preventDefault();
+        }
+      });
+    };
+});
+
+pokemonControllers.directive('ngArrowdown', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+          if(event.which === 40) {
+            scope.$apply(function (){
+              scope.$eval(attrs.ngArrowdown);
+            });
+            event.preventDefault();
+          }
+        });
+    };
+});
+
+pokemonControllers.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+          if(event.which === 40) {
+            scope.$apply(function (){
+              scope.$eval(attrs.ngEnter);
+            });
+            event.preventDefault();
+          }
+        });
+    };
+});
